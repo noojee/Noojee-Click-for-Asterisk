3f1cc582589d3ca1548af5f0f922391ec8ec0d1e
bsutton@noojeeit.com.au <bsutton@noojeeit.com.au@d7f9bb7b-1118-0410-857c-a3978f5fff11>
2010-06-21 12:30:00 +0000

    initial (and untested) work on moving the asterisk password into mozilla's secure storage.

--- srcExtension/content/job.js
+++ srcExtension/content/job.js
@@ -588,8 +588,8 @@ Login: function ()
 	{
 		theApp.util.njdebug("job", "login");
 		var url = theApp.job.genURL("Login");
-		var username = theApp.prefs.getValue("username");
-		var password = theApp.prefs.getValue("password"); 
+		var username = theApp.prefs.getUsername();
+		var password = theApp.prefs.getPassword();
 		theApp.asterisk.getInstance().setLoggedIn(false);
 		
 		theApp.util.njdebug("job", "Username: " + username);
