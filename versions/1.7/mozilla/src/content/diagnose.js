69bc2a208aacc58d94a12fb7a33fede0c4b08391
bsutton@noojeeit.com.au <bsutton@noojeeit.com.au@d7f9bb7b-1118-0410-857c-a3978f5fff11>
2011-06-26 23:20:59 +0000

    beta release of 1.7

---
+++ versions/1.7/mozilla/src/content/diagnose.js
@@ -0,0 +1,19 @@
+
+noojeeClick.ns(function() { with (noojeeClick.LIB) {
+
+theApp.diagnose =
+{
+
+onDiagnoseLoaded: function ()
+{
+	var diagConsole = window.document.getElementById('njDiagConsole');
+	
+	diagConsole.addText("Sorry, the connection tester isn't implemented as yet :< .")
+	//Asterisk.test()
+
+}
+
+
+}
+
+}});
