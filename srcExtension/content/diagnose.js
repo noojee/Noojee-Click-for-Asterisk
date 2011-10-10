6487d801559c4ef02bfaa9d0b57aa569ffade265
bsutton@noojeeit.com.au <bsutton@noojeeit.com.au@d7f9bb7b-1118-0410-857c-a3978f5fff11>
2011-10-10 02:40:48 +0000

    Added logic to remove an error on shutdown.

--- srcExtension/content/diagnose.js
+++ srcExtension/content/diagnose.js
@@ -1,19 +1,24 @@
-
-noojeeClick.ns(function() { with (noojeeClick.LIB) {
-
-theApp.diagnose =
+noojeeClick.ns(function()
 {
+	with (noojeeClick.LIB)
+	{
 
-onDiagnoseLoaded: function ()
-{
-	var diagConsole = window.document.getElementById('njDiagConsole');
-	
-	diagConsole.addText("Sorry, the connection tester isn't implemented as yet :< .")
-	//Asterisk.test()
+		theApp.diagnose =
+		{
 
-}
+			onDiagnoseLoaded : function()
+			{
+				if (window.document != null)
+				{
+					var diagConsole = window.document.getElementById('njDiagConsole');
 
+					diagConsole.addText("Sorry, the connection tester isn't implemented as yet :< .");
+					// Asterisk.test()
+				}
 
-}
+			}
 
-}});
+		};
+
+	}
+});
