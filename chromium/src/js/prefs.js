3dd9d3b6e2e70ad95dff50b036ff8fd9ce879fad
bsutton@noojee.com.au <bsutton@noojee.com.au>
2015-01-09 11:32:03 +1100

    Added first time initialisation to the preferences system.

--- chromium/src/js/prefs.js
+++ chromium/src/js/prefs.js
@@ -18,6 +18,7 @@
  * with Noojee Click. If not, see http://www.gnu.org/licenses/.
  **/
 
+
 noojeeClick.ns(function() { with (noojeeClick.LIB) {
 
 theApp.prefs =
@@ -63,6 +64,41 @@ theApp.prefs =
 	{
 		return theApp.prefs.getValue("password");	
 	},
+	
+	/** 
+	 * Used on a first time install (and perhaps on an update)
+	 * to set defaults for local storage.
+	 * The default values are created by 'prefs.js' under the
+	 * key prefix extensions.noojeeclick.
+	 * 
+	 * This method takes those defaults and puts them into the correct local storage.
+	 * 
+	 * We do this two phase method to be compatible with firefoxes 'pref' method for setting up defaults.
+	 * The problem with the firefox method under chrome is that we have no way of controling when the 
+	 * defaults/preferencespreferences.js script runs so it runs every time the extension starts (by inclusion in background.html)
+	 * rather than just on install or update.
+	 * So initPrefs is only called on install or update to copy any defaults into localstorage. This stops
+	 * localstorage being overwritten all of the time.
+	 */
+	initPrefs: function()
+	{
+		for (var i=0; i < localStorage.length; i++) 
+		{
+			var key = localStorage.key(i);
+			var value = localStorage[key];
+
+			var prefix = "z.defaults.noojeeclick.";
+			var len = prefix.length;
+			// Is this a default
+			if (key.indexOf(prefix) == 0)
+			{
+				// yes so transfer it.
+				key = key.substring(len);
+				localStorage[key] = value;
+				console.log(key + " : " + value);
+			}
+		}
+	}
 
 };
 
